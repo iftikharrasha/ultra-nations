afb69802173fc52651d189d819f125f9ed3e259c
iftikharrasha <iftikhar.rasha@northsouth.edu>
2021-04-13 07:16:42 +0600

    api data fetched

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [counties, setCountries] = useState([]);
+
+  useEffect(() => {
+    fetch('https://restcountries.eu/rest/v2/all')
+    .then(res => res.json())
+    .then(data => setCountries(data))
+  }, [])
+
   return (
     <div className="App">
+      <h1>Country loaded: {counties.length}</h1>
+        
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
